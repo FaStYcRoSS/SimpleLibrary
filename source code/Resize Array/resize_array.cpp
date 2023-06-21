115065e53548c20abafd1bb4f687a99e45b33264
FaStYcRoSS <tnazhmetdinov12@gmail.com>
2023-06-21 14:03:40 +0500

    make a iterators

--- source code/Resize Array/resize_array.cpp
+++ source code/Resize Array/resize_array.cpp
@@ -1,9 +1,12 @@
 #include <cstddef>
 #include <cstdlib>
 namespace simple {
-template <class type> 
-class resize_array {
+template <class type> class resize_array {
   type *data;
-  size_t size;
+  size_t capacity;
+
+public:
+  using iterator = type *;
+  using const_iterator = const type *;
 }
 } // namespace simple
